50a6bcf048c285537bec9349f772ca796ad8e4c1
lollo_pagno <lorenzo.pagnini@yahoo.it>
2022-02-11 11:33:29 +0100

    Created success form

---
+++ VitalSignsMonitorSimulator/Common/View/SuccessForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Common.View
+{
+    public partial class SuccessForm : Form
+    {
+        private const string ID_LABEL = "LabelTextError";
+
+        public SuccessForm()
+        {
+            InitializeComponent();
+        }
+
+        public void SetSext(string textError)
+        {
+            Label errorLabel = this.Controls.Find(ID_LABEL, true).FirstOrDefault() as Label;
+            errorLabel.Text = textError;
+            this.Show();
+        }
+
+        private void ButtonConfirmationClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
