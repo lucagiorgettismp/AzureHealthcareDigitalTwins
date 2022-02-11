c44227a7cad1b5756ab6d4ec6e29e997335ac1b7
lollo_pagno <lorenzo.pagnini@yahoo.it>
2022-02-11 15:59:07 +0100

    Binding obj c# - unity

--- HololensClient/HealthcareHololens/Assets/Script/View/VitalSignsMonitorView.cs
+++ HololensClient/HealthcareHololens/Assets/Script/View/VitalSignsMonitorView.cs
@@ -4,26 +4,89 @@ using System;
 
 public class VitalSignsMonitorView : VitalSignsMonitorElement
 {
-    public TextMeshProUGUI TemperatureValue;
-    public TextMeshProUGUI SaturationValue;
-    public TextMeshProUGUI BloodPressureValue;
-    public TextMeshProUGUI HeartFrequencyValue;
-    public TextMeshProUGUI BreathFrequencyValue;
-    public TextMeshProUGUI BatteryValue;
+    /* Value */
+    public TextMeshPro TemperatureValue;
+    public TextMeshPro SaturationValue;
+    public TextMeshPro BloodPressureValue;
+    public TextMeshPro HeartFrequencyValue;
+    public TextMeshPro BreathFrequencyValue;
+    public TextMeshPro BatteryValue;
+
+    /* Sensor name */
+    public TextMeshPro TemperatureSensorName;
+    public TextMeshPro SaturationSensorName;
+    public TextMeshPro BloodPressureSensorName;
+    public TextMeshPro HeartFrequencySensorName;
+    public TextMeshPro BreathFrequencySensorName;
+    public TextMeshPro BatterySensorName;
+
+    /* Symbol */
+    public TextMeshPro TemperatureSymbol;
+    public TextMeshPro SaturationSymbol;
+    public TextMeshPro BloodPressureSymbol;
+    public TextMeshPro HeartFrequencySymbol;
+    public TextMeshPro BreathFrequencySymbol;
+    public TextMeshPro BatterySymbol;
+
+    /* Alert */
+    public GameObject TemperatureAlert;
+    public GameObject SaturationAlert;
+    public GameObject BloodPressureAlert;
+    public GameObject HeartFrequencyAlert;
+    public GameObject BreathFrequencyAlert;
+    public GameObject BatteryAlert;
 
     public void UpdateView(Message message)
     {
         try
         {
-            TemperatureValue.text = message.TemperatureSensorName + ": " + Math.Round(message.TemperatureSensorValue.Value, 1);
-            SaturationValue.text = message.SaturationSensorName + ": " + message.SaturationSensorValue.Value;
-            BloodPressureValue.text = message.BloodPressureSensorName + ": " + message.BloodPressureSensorValue.Value;
-            HeartFrequencyValue.text = message.HeartFrequencySensorName + ": " + message.HeartFrequencySensorValue.Value;
-            BreathFrequencyValue.text = message.BreathFrequencySensorName + ": " + message.BreathFrequencySensorValue.Value;
-            BatteryValue.text = message.BatterySensorName + ": " + message.BatterySensorValue.Value;
+            UpdateSensorNames(message);
+            UpdateSensorSymbols(message);
+            UpdateSensorAlerts(message);
+            UpdateSensorValues(message);
         }catch(Exception e)
         {
             Debug.LogError("Error: " + e.Message);
         }
+    }
+
+    private void UpdateSensorSymbols(Message message) {
+        //TemperatureSymbol.text = message.TemperatureSensorValue.Symbol;
+        SaturationSymbol.text = message.SaturationSensorValue.Symbol;
+        BloodPressureSymbol.text = message.BloodPressureSensorValue.Symbol;
+        HeartFrequencySymbol.text = message.HeartFrequencySensorValue.Symbol;
+        BreathFrequencySymbol.text = message.BreathFrequencySensorValue.Symbol;
+        //BatterySymbol.text = message.BatterySensorValue.Symbol;
+    }
+
+    private void UpdateSensorValues(Message message) {
+        //TemperatureValue.text = Math.Round(message.TemperatureSensorValue.Value, 1).ToString();
+        SaturationValue.text =  message.SaturationSensorValue.Value.ToString();
+        BloodPressureValue.text = message.BloodPressureSensorValue.Value.ToString();
+        HeartFrequencyValue.text = message.HeartFrequencySensorValue.Value.ToString();
+        BreathFrequencyValue.text = message.BreathFrequencySensorValue.Value.ToString();
+        //BatteryValue.text = message.BatterySensorValue.Value.ToString();
+    }
+
+    private void UpdateSensorNames(Message message)
+    {
+        //TemperatureSensorName.text = message.TemperatureSensorName;
+        SaturationSensorName.text = message.SaturationSensorName;
+        BloodPressureSensorName.text = message.BloodPressureSensorName;
+        HeartFrequencySensorName.text = message.HeartFrequencySensorName;
+        BreathFrequencySensorName.text = message.BreathFrequencySensorName;
+        //BatterySensorName.text = message.BatterySensorName;
+    }
+    private void UpdateSensorAlerts(Message message) {
+
+        Material redColor = Resources.Load("Materials/RedColor", typeof(Material)) as Material;
+        Material whiteColor = Resources.Load("Materials/WhiteColor", typeof(Material)) as Material;
+
+        //TemperatureAlert.GetComponent<Renderer>().material = message.TemperatureSensorAlarm ? redColor : whiteColor;
+        SaturationAlert.GetComponent<Renderer>().material = message.SaturationSensorAlarm ? redColor : whiteColor;
+        BloodPressureAlert.GetComponent<Renderer>().material = message.BloodPressureSensorAlarm ? redColor : whiteColor;
+        HeartFrequencyAlert.GetComponent<Renderer>().material = message.HeartFrequencySensorAlarm ? redColor : whiteColor;
+        BreathFrequencyAlert.GetComponent<Renderer>().material = message.BreathFrequencySensorAlarm ? redColor : whiteColor;
+        //BatteryAlert.GetComponent<Renderer>().material = message.BatterySensorAlarm ? redColor : whiteColor;
     }
 }
