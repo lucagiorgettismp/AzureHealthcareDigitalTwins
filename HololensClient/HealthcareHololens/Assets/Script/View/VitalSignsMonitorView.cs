e89cd8d4620cb80afc7344ecf7bb95723f808515
lollo_pagno <lorenzo.pagnini@yahoo.it>
2022-02-14 16:31:05 +0100

    Improved binding c# object

--- HololensClient/HealthcareHololens/Assets/Script/View/VitalSignsMonitorView.cs
+++ HololensClient/HealthcareHololens/Assets/Script/View/VitalSignsMonitorView.cs
@@ -5,43 +5,38 @@ using System;
 public class VitalSignsMonitorView : VitalSignsMonitorElement
 {
     /* Value */
-    public TextMeshPro TemperatureValue;
-    public TextMeshPro SaturationValue;
-    public TextMeshPro BloodPressureValue;
-    public TextMeshPro HeartFrequencyValue;
-    public TextMeshPro BreathFrequencyValue;
-    public TextMeshPro BatteryValue;
+    private TextMeshPro TemperatureValue;
+    private TextMeshPro SaturationValue;
+    private TextMeshPro BloodPressureValue;
+    private TextMeshPro HeartFrequencyValue;
+    private TextMeshPro BreathFrequencyValue;
+    private TextMeshPro BatteryValue;
 
     /* Sensor name */
-    public TextMeshPro TemperatureSensorName;
-    public TextMeshPro SaturationSensorName;
-    public TextMeshPro BloodPressureSensorName;
-    public TextMeshPro HeartFrequencySensorName;
-    public TextMeshPro BreathFrequencySensorName;
-    public TextMeshPro BatterySensorName;
+    private TextMeshPro TemperatureSensorName;
+    private TextMeshPro SaturationSensorName;
+    private TextMeshPro BloodPressureSensorName;
+    private TextMeshPro HeartFrequencySensorName;
+    private TextMeshPro BreathFrequencySensorName;
+    private TextMeshPro BatterySensorName;
 
     /* Symbol */
-    public TextMeshPro TemperatureSymbol;
-    public TextMeshPro SaturationSymbol;
-    public TextMeshPro BloodPressureSymbol;
-    public TextMeshPro HeartFrequencySymbol;
-    public TextMeshPro BreathFrequencySymbol;
-    public TextMeshPro BatterySymbol;
+    private TextMeshPro TemperatureSymbol;
+    private TextMeshPro SaturationSymbol;
+    private TextMeshPro BloodPressureSymbol;
+    private TextMeshPro HeartFrequencySymbol;
+    private TextMeshPro BreathFrequencySymbol;
+    private TextMeshPro BatterySymbol;
 
     /* Alert */
-    public GameObject TemperatureAlert;
-    public GameObject SaturationAlert;
-    public GameObject BloodPressureAlert;
-    public GameObject HeartFrequencyAlert;
-    public GameObject BreathFrequencyAlert;
-    public GameObject BatteryAlert;
+    private GameObject TemperatureAlert;
+    private GameObject SaturationAlert;
+    private GameObject BloodPressureAlert;
+    private GameObject HeartFrequencyAlert;
+    private GameObject BreathFrequencyAlert;
+    private GameObject BatteryAlert;
 
     /* Line Chart*/
-    public GameObject HeartFrequencyDiagram;
-    public GameObject BreathFrequencyDiagram;
-    public GameObject SaturationDiagram;
-    public GameObject BloodPressureDiagram;
-
     private DD_DataDiagram HeartFrequencyDataDiagram;
     private DD_DataDiagram BreathFrequencyDataDiagram;
     private DD_DataDiagram SaturationDataDiagram;
@@ -52,17 +47,71 @@ public class VitalSignsMonitorView : VitalSignsMonitorElement
     private GameObject BloodPressureLine;
     private GameObject SaturationLine;
 
+    /* Colors */
+    const string RED_COLOR = "Materials/RedColor";
+    const string WHITE_COLOR = "Materials/WhiteColor";
+
     public void Start()
     {
-        HeartFrequencyDataDiagram = HeartFrequencyDiagram.GetComponent<DD_DataDiagram>();
-        BreathFrequencyDataDiagram = BreathFrequencyDiagram.GetComponent<DD_DataDiagram>();
-        SaturationDataDiagram = SaturationDiagram.GetComponent<DD_DataDiagram>();
-        BloodPressureDataDiagram = BloodPressureDiagram.GetComponent<DD_DataDiagram>();
-    
-        this.HeartLine = this.HeartFrequencyDataDiagram.AddLine("HeartFrequency", Color.green);
-        this.BreathLine = this.BreathFrequencyDataDiagram.AddLine("BreathFrequency", Color.green);
-        this.SaturationLine = this.SaturationDataDiagram.AddLine("Saturation", Color.red);
-        this.BloodPressureLine = this.BloodPressureDataDiagram.AddLine("BloodPressure", Color.yellow);
+        InitializedComponent();
+    }
+
+    private void InitializedComponent()
+    {
+        /* Value components */
+        this.HeartFrequencyValue    = GameObject.Find("HeartFrequencyValue").GetComponent<TextMeshPro>();
+        this.BreathFrequencyValue   = GameObject.Find("BreathFrequencyValue").GetComponent<TextMeshPro>();
+        this.SaturationValue        = GameObject.Find("SaturationValue").GetComponent<TextMeshPro>();
+        this.HeartFrequencyValue    = GameObject.Find("HeartFrequencyValue").GetComponent<TextMeshPro>();
+        this.BloodPressureValue     = GameObject.Find("BloodPressureValue").GetComponent<TextMeshPro>();
+        //this.TemperatureValue     = GameObject.Find("TemperatureValue").GetComponent<TextMeshPro>();
+        //this.BatteryValue         = GameObject.Find("BatteryValue").GetComponent<TextMeshPro>();
+
+
+        /* Symbol components */
+        this.HeartFrequencySymbol   = GameObject.Find("HeartFrequencySymbol").GetComponent<TextMeshPro>();
+        this.BreathFrequencySymbol  = GameObject.Find("BreathFrequencySymbol").GetComponent<TextMeshPro>();
+        this.SaturationSymbol       = GameObject.Find("SaturationSymbol").GetComponent<TextMeshPro>();
+        this.HeartFrequencySymbol   = GameObject.Find("HeartFrequencySymbol").GetComponent<TextMeshPro>();
+        this.BloodPressureSymbol    = GameObject.Find("BloodPressureSymbol").GetComponent<TextMeshPro>();
+        //this.TemperatureSymbol    = GameObject.Find("TemperatureSymbol").GetComponent<TextMeshPro>();
+        //this.BatterySymbol        = GameObject.Find("BatterySymbol").GetComponent<TextMeshPro>();
+
+        /* Sensor name components */
+        this.HeartFrequencySensorName   = GameObject.Find("HeartFrequencySensorName").GetComponent<TextMeshPro>();
+        this.BreathFrequencySensorName  = GameObject.Find("BreathFrequencySensorName").GetComponent<TextMeshPro>();
+        this.SaturationSensorName       = GameObject.Find("SaturationSensorName").GetComponent<TextMeshPro>();
+        this.HeartFrequencySensorName   = GameObject.Find("HeartFrequencySensorName").GetComponent<TextMeshPro>();
+        this.BloodPressureSensorName    = GameObject.Find("BloodPressureSensorName").GetComponent<TextMeshPro>();
+        //this.TemperatureSensorName    = GameObject.Find("TemperatureSensorName").GetComponent<TextMeshPro>();
+        //this.BatterySensorName        = GameObject.Find("BatterySensorName").GetComponent<TextMeshPro>();
+
+        /* Alert components */
+        this.HeartFrequencyAlert    = GameObject.Find("HeartFrequencyAlert");
+        this.BreathFrequencyAlert   = GameObject.Find("BreathFrequencyAlert");
+        this.SaturationAlert        = GameObject.Find("SaturationAlert");
+        this.HeartFrequencyAlert    = GameObject.Find("HeartFrequencyAlert");
+        this.BloodPressureAlert     = GameObject.Find("BloodPressureAlert");
+        //this.TemperatureAlert     = GameObject.Find("TemperatureAlert");
+        //this.BatteryAlert         = GameObject.Find("BatteryAlert");
+
+        Material whiteColor = Resources.Load(WHITE_COLOR, typeof(Material)) as Material;
+        this.SaturationAlert.GetComponent<Renderer>().material = whiteColor;
+        this.BloodPressureAlert.GetComponent<Renderer>().material = whiteColor;
+        this.HeartFrequencyAlert.GetComponent<Renderer>().material = whiteColor;
+        this.BreathFrequencyAlert.GetComponent<Renderer>().material = whiteColor;
+
+        /* Line chart components */
+
+        this.HeartFrequencyDataDiagram  = GameObject.Find("HeartFrequencyLineChart").GetComponent<DD_DataDiagram>();
+        this.BreathFrequencyDataDiagram = GameObject.Find("BreathFrequencyLineChart").GetComponent<DD_DataDiagram>();
+        this.SaturationDataDiagram      = GameObject.Find("SaturationLineChart").GetComponent<DD_DataDiagram>();
+        this.BloodPressureDataDiagram   = GameObject.Find("BloodPressureLineChart").GetComponent<DD_DataDiagram>();
+
+        this.HeartLine          = this.HeartFrequencyDataDiagram.AddLine("HeartFrequency", Color.green);
+        this.BreathLine         = this.BreathFrequencyDataDiagram.AddLine("BreathFrequency", Color.green);
+        this.SaturationLine     = this.SaturationDataDiagram.AddLine("Saturation", Color.red);
+        this.BloodPressureLine  = this.BloodPressureDataDiagram.AddLine("BloodPressure", Color.yellow);
     }
 
     public void UpdateView(Message message)
@@ -81,43 +130,43 @@ public class VitalSignsMonitorView : VitalSignsMonitorElement
     }
 
     private void UpdateSensorSymbols(Message message) {
-        //this.TemperatureSymbol.text = message.TemperatureSensorValue.Symbol;
-        this.SaturationSymbol.text = message.SaturationSensorValue.Symbol;
-        this.BloodPressureSymbol.text = message.BloodPressureSensorValue.Symbol;
-        this.HeartFrequencySymbol.text = message.HeartFrequencySensorValue.Symbol;
+        //this.TemperatureSymbol.text   = message.TemperatureSensorValue.Symbol;
+        this.SaturationSymbol.text      = message.SaturationSensorValue.Symbol;
+        this.BloodPressureSymbol.text   = message.BloodPressureSensorValue.Symbol;
+        this.HeartFrequencySymbol.text  = message.HeartFrequencySensorValue.Symbol;
         this.BreathFrequencySymbol.text = message.BreathFrequencySensorValue.Symbol;
-        //this.BatterySymbol.text = message.BatterySensorValue.Symbol;
+        //this.BatterySymbol.text       = message.BatterySensorValue.Symbol;
     }
 
     private void UpdateSensorValues(Message message) {
-        //this.TemperatureValue.text = Math.Round(message.TemperatureSensorValue.Value, 1).ToString();
-        this.SaturationValue.text =  message.SaturationSensorValue.Value.ToString();
-        this.BloodPressureValue.text = message.BloodPressureSensorValue.Value.ToString();
-        this.HeartFrequencyValue.text = message.HeartFrequencySensorValue.Value.ToString();
-        this.BreathFrequencyValue.text = message.BreathFrequencySensorValue.Value.ToString();
-        //this.BatteryValue.text = message.BatterySensorValue.Value.ToString();
+        //this.TemperatureValue.text    = Math.Round(message.TemperatureSensorValue.Value, 1).ToString();
+        this.SaturationValue.text       =  message.SaturationSensorValue.Value.ToString();
+        this.BloodPressureValue.text    = message.BloodPressureSensorValue.Value.ToString();
+        this.HeartFrequencyValue.text   = message.HeartFrequencySensorValue.Value.ToString();
+        this.BreathFrequencyValue.text  = message.BreathFrequencySensorValue.Value.ToString();
+        //this.BatteryValue.text        = message.BatterySensorValue.Value.ToString();
     }
 
     private void UpdateSensorNames(Message message)
     {
-        //this.TemperatureSensorName.text = message.TemperatureSensorName;
-        this.SaturationSensorName.text = message.SaturationSensorName;
-        this.BloodPressureSensorName.text = message.BloodPressureSensorName;
-        this.HeartFrequencySensorName.text = message.HeartFrequencySensorName;
+        //this.TemperatureSensorName.text   = message.TemperatureSensorName;
+        this.SaturationSensorName.text      = message.SaturationSensorName;
+        this.BloodPressureSensorName.text   = message.BloodPressureSensorName;
+        this.HeartFrequencySensorName.text  = message.HeartFrequencySensorName;
         this.BreathFrequencySensorName.text = message.BreathFrequencySensorName;
-        //this.BatterySensorName.text = message.BatterySensorName;
+        //this.BatterySensorName.text       = message.BatterySensorName;
     }
     private void UpdateSensorAlerts(Message message) {
 
-        Material redColor = Resources.Load("Materials/RedColor", typeof(Material)) as Material;
-        Material whiteColor = Resources.Load("Materials/WhiteColor", typeof(Material)) as Material;
+        Material redColor = Resources.Load(RED_COLOR, typeof(Material)) as Material;
+        Material whiteColor = Resources.Load(WHITE_COLOR, typeof(Material)) as Material;
 
-        //this.TemperatureAlert.GetComponent<Renderer>().material = message.TemperatureSensorAlarm ? redColor : whiteColor;
-        this.SaturationAlert.GetComponent<Renderer>().material = message.SaturationSensorAlarm ? redColor : whiteColor;
-        this.BloodPressureAlert.GetComponent<Renderer>().material = message.BloodPressureSensorAlarm ? redColor : whiteColor;
-        this.HeartFrequencyAlert.GetComponent<Renderer>().material = message.HeartFrequencySensorAlarm ? redColor : whiteColor;
+        //this.TemperatureAlert.GetComponent<Renderer>().material   = message.TemperatureSensorAlarm ? redColor : whiteColor;
+        this.SaturationAlert.GetComponent<Renderer>().material      = message.SaturationSensorAlarm ? redColor : whiteColor;
+        this.BloodPressureAlert.GetComponent<Renderer>().material   = message.BloodPressureSensorAlarm ? redColor : whiteColor;
+        this.HeartFrequencyAlert.GetComponent<Renderer>().material  = message.HeartFrequencySensorAlarm ? redColor : whiteColor;
         this.BreathFrequencyAlert.GetComponent<Renderer>().material = message.BreathFrequencySensorAlarm ? redColor : whiteColor;
-        //this.BatteryAlert.GetComponent<Renderer>().material = message.BatterySensorAlarm ? redColor : whiteColor;
+        //this.BatteryAlert.GetComponent<Renderer>().material       = message.BatterySensorAlarm ? redColor : whiteColor;
     }
 
     private void UpdateLineCharts(Message message)
