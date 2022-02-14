cbe8ef0e7a661771dbed36e59cf0c9232613a0d3
Luca Giorgetti <lucagiorgit95@gmail.com>
2022-02-14 20:26:33 +0100

    Support for y axis minvalue and maxvalue

--- HololensClient/HealthcareHololens/Assets/WindowGraph.cs
+++ HololensClient/HealthcareHololens/Assets/WindowGraph.cs
@@ -6,24 +6,33 @@ using System;
 
 public class WindowGraph : MonoBehaviour
 {
+    [SerializeField] private Sprite circleSprite;
+    [SerializeField] public int yAxisMin;
+    [SerializeField] public float yAxisMax;
+    [SerializeField] public int ySeparators;
+    [SerializeField] public int xPoints;
+
+
     // Start is called before the first frame update
     private RectTransform graphContainer;
-    private RectTransform labelTemplateX;
     private RectTransform labelTemplateY;
-    private RectTransform dashTemplateX;
     private RectTransform dashTemplateY;
 
-    [SerializeField] private Sprite circleSprite;
+    //private RectTransform labelTemplateX;
+    //private RectTransform dashTemplateX;
 
     private void Awake()
     {
         graphContainer = transform.Find("GraphContainer").GetComponent<RectTransform>();
-        labelTemplateX = graphContainer.Find("LabelTemplateX").GetComponent<RectTransform>();
         labelTemplateY = graphContainer.Find("LabelTemplateY").GetComponent<RectTransform>();
-        dashTemplateX = graphContainer.Find("DashTemplateX").GetComponent<RectTransform>();
         dashTemplateY = graphContainer.Find("DashTemplateY").GetComponent<RectTransform>();
+       
+        //labelTemplateX = graphContainer.Find("LabelTemplateX").GetComponent<RectTransform>();
+        //dashTemplateX = graphContainer.Find("DashTemplateX").GetComponent<RectTransform>();
 
-        var values = new List<int>() { 0, 15, 18, 56, 26, 48, 89, 56, 26, 48, 89, 56, 33, 100 };
+        //var values = new List<int>() { 0, 15, 18, 56, 26, 48, 89, 56, 26, 48, 89, 56, 33, 100 }; //0-100
+        var values = new List<int>() { 70, 75, 95, 120, 100, 85, 75, 70, 85, 90, 95, 100, 115, 120, 115, 100, 85, 80 }; //70-120
+
         ShowGraph(values);
     }
 
@@ -43,6 +52,7 @@ public class WindowGraph : MonoBehaviour
         GameObject gameObject = new GameObject("Circle", typeof(Image));
         gameObject.transform.SetParent(graphContainer, false);
         gameObject.GetComponent<Image>().sprite = circleSprite;
+        //GetComponent<SpriteRenderer>().color = Color.yellow;
 
         RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
         rectTransform.anchoredPosition = position;
@@ -56,15 +66,13 @@ public class WindowGraph : MonoBehaviour
     private void ShowGraph(List<int> values)
     {
         float graphHeight = graphContainer.sizeDelta.y;
-        float xSize = 20f;
-        float ySize = 50f;
-        float yMax = 100f;
+        float xSize = graphContainer.sizeDelta.x / (xPoints - 1);
 
         GameObject lastCircleGameObject = null;
         for (int i = 0; i < values.Count; i++)
         {
-            float xPos = xSize + (i * xSize);
-            float yPos = (values[i] / yMax) * graphHeight;
+            float xPos = i * xSize;
+            float yPos = ((values[i] - yAxisMin) / (yAxisMax-yAxisMin)) * graphHeight;
             var circleGameObject = CreateCirlce(new Vector2(xPos, yPos));
 
             if (lastCircleGameObject != null)
@@ -88,16 +96,14 @@ public class WindowGraph : MonoBehaviour
             */
         }
 
-        int separatorCount = 10;
-
-        for (int i = 0; i <= separatorCount; i++)
+        for (int i = 0; i <= ySeparators; i++)
         {
             var labelY = Instantiate(labelTemplateY);
             labelY.SetParent(graphContainer, false);
             labelY.gameObject.SetActive(true);
-            float normalizedValue = i * 1f / separatorCount;
+            float normalizedValue = i * 1f / ySeparators;
             labelY.anchoredPosition = new Vector2(-10f, normalizedValue * graphHeight);
-            labelY.GetComponent<Text>().text = Convert.ToInt32(normalizedValue * yMax).ToString();
+            labelY.GetComponent<Text>().text = Convert.ToInt32(yAxisMin + (normalizedValue * (yAxisMax-yAxisMin))).ToString();
 
             var dashY = Instantiate(dashTemplateY);
             dashY.SetParent(graphContainer, false);
